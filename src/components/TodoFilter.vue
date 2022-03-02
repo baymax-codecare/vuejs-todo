63f74acc7fb0e505a42bbf284dd1caa2c7d2083c
baymax-codecare <david.lee4015@gmail.com>
2022-03-02 09:38:50 -0800

    filterComponent

---
+++ src/components/TodoFilter.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flex flex-row items-center">
+    <label>Filter</label>
+    <select
+      class="
+        form-select form-select-sm
+        appearance-none
+        w-30
+        px-3
+        my-2
+        mx-2
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding bg-no-repeat
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        focus:text-gray-700
+        focus:bg-white
+        focus:border-blue-600
+        focus:outline-none
+      "
+      aria-label="Default select example"
+      @change="change($event.target.value)"
+    >
+      <option
+        v-for="filterType in filterTypes"
+        :key="filterType"
+        :value="filterType"
+      >
+        {{ filterType }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    setFilter: { type: Function },
+  },
+  data: function () {
+    return { filterTypes: ["All", "Incomplete", "Completed"] };
+  },
+  methods:{
+    change(value){
+      this.setFilter(value)
+    }
+  },
+};
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -15,42 +15,7 @@
         <h1 class="text-grey-darkest">Todo List</h1>
         <TodoInput />
       </div>
-      <div class="flex flex-row items-center">
-        <label>Filter</label>
-        <select
-          class="
-            form-select form-select-sm
-            appearance-none
-            w-30
-            px-3
-            my-2
-            mx-2
-            py-1.5
-            text-base
-            font-normal
-            text-gray-700
-            bg-white bg-clip-padding bg-no-repeat
-            border border-solid border-gray-300
-            rounded
-            transition
-            ease-in-out
-            focus:text-gray-700
-            focus:bg-white
-            focus:border-blue-600
-            focus:outline-none
-          "
-          aria-label="Default select example"
-          @change="setFilter($event.target.value)"
-        >
-          <option
-            v-for="filterType in filterTypes"
-            :key="filterType"
-            :value="filterType"
-          >
-            {{ filterType }}
-          </option>
-        </select>
-      </div>
+      <TodoFilter :set-filter = "setFilter" />
       <div class="list-container">
         <TodoItem v-for="todo in todos" :key="todo.id" :task="todo" />
       </div>
@@ -61,17 +26,17 @@
 <script>
 import TodoInput from "./TodoInput.vue";
 import TodoItem from "./TodoItem.vue";
+import TodoFilter from "./TodoFilter.vue";
 
 import { useTodoList } from "../composables/useTodoList";
 
 export default {
-  components: { TodoInput, TodoItem },
+  components: { TodoInput, TodoItem, TodoFilter },
   setup() {
     const { setFilter, todos, filter } = useTodoList();
     return {
       todos,
       filter,
-      filterTypes: ["All", "Incomplete", "Completed"],
       setFilter,
     };
   },
